74098280a053201e64567dc7d6860e3a3c9227ba
Kevin Le <lekevin42@yahoo.com>
2017-03-22 20:49:55 -0700

    Remove comments

--- scripts/validation.js
+++ scripts/validation.js
@@ -15,13 +15,6 @@
         }
     };
 
-    /*    function decaf_verify(s_val, caffeine_level) {
-            if (s_val.indexOf('decaf') != -1 && caffeine_level > 20){
-              return false;
-            }
-            return true;
-        }
-    */
     App.Validation = Validation;
     window.App = App;
 })(window);
